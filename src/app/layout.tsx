472e5bd312cadca087dc2b0bfbaab5cda707dacf
CristianGaudino <gaudino.cristian@gmail.com>
2026-04-09 22:19:41 +0100

    Added a simple favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,12 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="h-full">
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
+        <link rel="manifest" href="/site.webmanifest"></link>
+      </head>
       <body className="min-h-full flex flex-col bg-[#0a0a0f] text-white">
         {children}
       </body>
